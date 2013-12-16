fd4fe7a5f4a175a7571f28fbfea3becaa86b6376
Scott Lanning <slanning@cpan.org>
2013-12-16 12:52:03 +0100

    prep for 0.14 CPAN release

--- lib/Net/Graphite.pm
+++ lib/Net/Graphite.pm
@@ -6,7 +6,7 @@ use Carp qw/confess/;
 use IO::Socket::INET;
 use Scalar::Util qw/reftype/;
 
-$Net::Graphite::VERSION = '0.13';
+$Net::Graphite::VERSION = '0.14';
 
 our $TEST = 0;   # if true, don't send anything to graphite
 
